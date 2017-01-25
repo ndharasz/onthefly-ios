efa8432ac4859490f47a408be191090b25303be8
Scott Higgins <skoty.higgins@gmail.com>
2017-01-25 11:32:23 -0500

    Create account UI components, Style object created to simplify color management for theme of app

---
+++ On The Fly/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  On The Fly
+//
+//  Created by Scott Higgins on 1/25/17.
+//  Copyright © 2017 Team 152 - Easily The Best. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
