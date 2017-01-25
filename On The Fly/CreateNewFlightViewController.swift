5a7e299199e7ee63c4736a423d0d010d161a9c61
Scott Higgins <skoty.higgins@gmail.com>
2017-01-25 00:21:25 -0500

    Basic ui setup (plane picker, data picker, airport entries, submit and cancel buttons)

---
+++ On The Fly/CreateNewFlightViewController.swift
@@ -0,0 +1,56 @@
+//
+//  CreateNewFlightViewController.swift
+//  On The Fly
+//
+//  Created by Scott Higgins on 1/24/17.
+//  Copyright © 2017 Team 152 - Easily The Best. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewFlightViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var planePicker: UIPickerView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    var pickerData: [String] = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        pickerData = ["Piper Saratoga N736X", "King Air N799F", "Cessna Citation N899O"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Picker View Code
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
