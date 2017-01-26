54813ff0af90630090c05d368fe1ff6beda4f61d
Scott Higgins <skoty.higgins@gmail.com>
2017-01-25 21:26:15 -0500

    Collection view of "passenger seats" added Keyboard hiding extension added for text entry Page indicator added to edit flight screen

--- On The Fly/EditFlightViewController.swift
+++ On The Fly/EditFlightViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class EditFlightViewController: UIViewController {
+class EditFlightViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var passengerCollectionView: UICollectionView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pageControl.numberOfPages = 2
+        pageControl.currentPage = 1
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Collection View Code
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = passengerCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = Style.darkBlueAccentColor
+        return cell
     }
     
 
