d232b1e9354d38731a485015ae994fdae5aa33c3
Scott Higgins <skoty.higgins@gmail.com>
2017-01-25 22:41:21 -0500

    Pop-up dialogue implemented, selecting default option returns to home-screen

--- On The Fly/ForgotPasswordViewController.swift
+++ On The Fly/ForgotPasswordViewController.swift
@@ -13,7 +13,6 @@ class ForgotPasswordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +20,18 @@ class ForgotPasswordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func resetButtonPressed(_ sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "Password Successfully Reset", message: "You have successfully reset the password. Select 'Okay' to return to the home screen.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: {action in
+            
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+            
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
