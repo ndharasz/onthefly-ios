ef600a74ffb4a91f1eff23d2c6649f56d71278aa
Scott Higgins <skoty.higgins@gmail.com>
2017-01-26 21:53:39 -0500

    Initial view and view controller setup; basic transitions

---
+++ On The Fly/ReportGenerationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReportGenerationViewController.swift
+//  On The Fly
+//
+//  Created by Scott Higgins on 1/26/17.
+//  Copyright © 2017 Team 152 - Easily The Best. All rights reserved.
+//
+
+import UIKit
+
+class ReportGenerationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
