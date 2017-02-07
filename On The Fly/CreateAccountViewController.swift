7f653ce31d7a1089940a03629a2cac3f95afec53
Callie Bivings <calliebivings@lawn-128-61-44-203.lawn.gatech.edu>
2017-02-07 12:52:08 -0500

    added email validity check and password matching check

--- On The Fly/CreateAccountViewController.swift
+++ On The Fly/CreateAccountViewController.swift
@@ -20,25 +20,59 @@ class CreateAccountViewController: UIViewController {
     var textFieldArray: [UITextField] = []
     
     @IBAction func createAccountButtonPressed(_ sender: UIButton) {
-//        text box variables
-        //        let userFirstName = firstNameTextField.text
-        //        let userLastName = lastNameTextField.text
-        let userEmail = emailTextField.text
-        let userPassword = passwordTextField.text
-        //        let userConfirmPassword = confirmPasswordTextField.text
-        FIRAuth.auth()?.createUser(withEmail: userEmail!, password: userPassword!, completion: { (user: FIRUser?, error) in
-            var title = ""
-            var message = ""
-            if error != nil {
-                title = "Oops!"
-                message = (error?.localizedDescription)!
-                self.alert(message: message, title: title)
+//      text box variables
+//        let userFirstName = firstNameTextField.text
+//        let userLastName = lastNameTextField.text
+        let userConfirmPassword = confirmPasswordTextField.text
+        if let userEmail = emailTextField.text, let userPassword = passwordTextField.text {
+            let validEmail = checkValidEmail(email: userEmail)
+            let validPassword = checkValidPassword(password: userPassword, confirmPassword: userConfirmPassword!)
+            if (validEmail) {
+                if (validPassword) {
+                    FIRAuth.auth()?.createUser(withEmail: userEmail, password: userPassword, completion: { (user: FIRUser?, error) in
+                        var title = ""
+                        var message = ""
+                        
+                        if error != nil {
+                            title = "Oops!"
+                            message = (error?.localizedDescription)!
+                            self.alert(message: message, title: title)
+                        } else {
+                            title = "Success!"
+                            message = "Account created."
+                            self.alertDismissView(message: message, title: title)
+                        }
+                    })
+                } else {
+                    //not a valid password combo
+                    alert(message: "The passwords you entered do not match, please try again.", title: "Passwords don't match")
+                }
             } else {
-                title = "Success!"
-                message = "Account created."
-                self.alertDismissView(message: message, title: title)
+                alert(message: "The email you entered is not valid, please check the email and try again", title: "Invalid email address")
             }
-        })
+        }
+    }
+    
+    //checking validity of email address
+    private func checkValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        if (email.characters.count == 0) {
+            return false
+        } else if (!emailTest.evaluate(with: email)) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    //checking validity of password and making sure passwords match
+    private func checkValidPassword(password: String, confirmPassword: String) -> Bool {
+        if (password == confirmPassword) {
+            return true
+        } else {
+            return false
+        }
     }
     
     override func viewDidLoad() {
